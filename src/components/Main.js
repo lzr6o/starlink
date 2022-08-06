a6f6f9fc864b6c0205fbe2535ac32946d5ce2741
李zr <zr6oli@gmail.com>
2022-08-06 19:29:41 -0400

    edit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,22 @@
-import React, {Component} from 'react';
-import { Row, Col } from 'antd';
-import SatSetting from './SatSetting';
+import React, { Component } from "react";
+import { Row, Col } from "antd";
+import SatSetting from "./SatSetting";
+import SatelliteList from "./SatelliteList";
 
 class Main extends Component {
-    render() {
-        return (
-            <Row className='main'>
-                <Col span={8} className="left-side">
-                    <SatSetting />
-                </Col>
-                <Col span={16} className="right-side">
-                    right
-                </Col>
-            </Row>
-        );
-    }
+  render() {
+    return (
+      <Row className="main">
+        <Col span={8} className="left-side">
+          <SatSetting />
+          <SatelliteList />
+        </Col>
+        <Col span={16} className="right-side">
+          right
+        </Col>
+      </Row>
+    );
+  }
 }
 
 export default Main;
